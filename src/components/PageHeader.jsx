86ba933344b6eed839f42957239f263b94d92596
bethy <bethyd123@gmail.com>
2017-01-20 00:29:22 -0500

    adding map and page header components

---
+++ src/components/PageHeader.jsx
@@ -0,0 +1,16 @@
+"use strict";
+
+import React, { Component } from "react";
+
+export default class PageHeader extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Women's Healthcare Availability in the U.S.</h1>
+        <p classname="subtitle">Many women in the United States rely on their
+        insurance providers for access to affordable healthcare.
+        What does this access look like?</p>
+      </div>
+    );
+  }
+}
---
+++ src/components/map/Map.jsx
@@ -0,0 +1,85 @@
+"use strict";
+
+import "../../styles/map.sass";
+import React, { Component } from "react";
+import $ from "jquery";
+import d3 from "d3";
+import Datamap from "datamaps";
+import categories from "../../data/categories.json";
+import Dispatcher from "../../setup.js";
+
+export default class Map extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentMounted() {
+    if (this.props.fetched) {
+      this.drawMap();
+    }
+  }
+
+  componentUpdated() {
+    if (this.props.fetched) {
+      this.drawMap();
+    }
+  }
+
+  render() {
+    return <div id="map"></div>;
+  }
+
+  drawMap() {
+    let $map = $("#map");
+    let darkenedColor = d3.rgb(this.props.colors.range(70)).darker();
+    $map.empty();
+
+    let data = {};
+    this.props.data.forEach((d) => {
+      if ((d[this.props.activeInsurance]) === "N/A") {
+        data[d.state] = {
+          fillColor: this.props.colors.defaultFill,
+          percent: "n/a"
+        };
+      } else {
+        data[d.state] = {
+          fillColor: this.props.colors.range(d[this.props.activeInsurance]),
+          percent: d[this.props.activeInsurance]
+        };
+      }
+    });
+
+    this.map = new Datamap({
+      element: $map[0],
+      done: (map) => {
+        map.svg.selectAll(".datamaps-subunit").on("click", function (geography) {
+          Dispatcher.dispatch({
+            actionType: "ACTIVE_STATE_CHANGED",
+            data: geography.id
+          });
+        });
+      },
+      scope: "usa",
+      fills: { defaultFill: this.props.colors.defaultFill },
+      data: data,
+      geographyConfig: {
+        borderColor: darkenedColor,
+        highlightFillColor: darkenedColor,
+        highlightBorderColor: darkenedColor,
+        hightlightBorderWidth: 1,
+        popupTemplate: (geo, d) => {
+          return "<div class=\"hoverinfo\"><b>" + geo.properties.name + "</b><br>" +
+          d.percent + "% of women " + categories.insurance[this.props.activeInsurance].have + "</div>";
+        }
+      }
+    });
+  }
+}
+
+Map.propTypes = {
+  fetched: React.PropTypes.bool.isRequired,
+  data: React.PropTypes.array,
+  activeState: React.Proptypes.string.isRequired,
+  activeInsurance: React.PropTypes.string.isRequired,
+  colors: React.PropTypes.object.isRequired
+};
